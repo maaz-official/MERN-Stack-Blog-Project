547fe035cf6b5ec7d307942a10494836aff08073
maaz-official <146274279+pwbcs0933@users.noreply.github.com>
2024-03-04 23:18:31 +0500

    Added React Router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <h1 className='text-center text-red-500'>Hello</h1>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <h1>Hello</h1>
   )
 }
 
-export default Home
+export default Home;
