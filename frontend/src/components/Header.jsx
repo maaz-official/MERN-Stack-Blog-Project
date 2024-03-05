ac2d2774e2a5e911949b5e25df5d32864d099d96
maaz-official <146274279+pwbcs0933@users.noreply.github.com>
2024-03-05 23:15:11 +0500

    change the Links style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,58 +1,89 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { Button, Navbar, TextInput } from 'flowbite-react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 export default function Header() {
   const path = useLocation().pathname;
   return (
-    <Navbar className='border-b-2 header'> {/* Added 'header' class */}
-    <Link to="/" className="flex items-center font-serif text-3xl font-bold text-gray-800">
-      <span className="text-yellow-500">L</span>
-      <span>a</span>
-      <span className="text-red-500">z</span>
-      <span>z</span>
-      <span className="text-green-500">y</span>
-      <span className="ml-1">Code</span>
-    </Link>
+    <Navbar className="border-b-2 header">
+      {" "}
+      {/* Added 'header' class */}
+      <Link
+        to="/"
+        className="flex items-center font-serif text-3xl font-bold text-gray-800"
+      >
+        <span className="text-yellow-500">L</span>
+        <span>a</span>
+        <span className="text-red-500">z</span>
+        <span>z</span>
+        <span className="text-green-500">y</span>
+        <span className="ml-1">Code</span>
+      </Link>
       <form>
-        <TextInput 
-          type='text'
-          placeholder='Search...'
+        <TextInput
+          type="text"
+          placeholder="Search..."
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
+          className="hidden lg:inline"
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      <div className='flex gap-2 md:order-2'> {/* Updated responsive classes */}
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+      <div className="flex gap-2 md:order-2">
+        {" "}
+        {/* Updated responsive classes */}
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to='/signin'>
-          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+        <Link to="/signin">
+          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
-      <Navbar.Collapse> {/* Collapsible Navbar section */}
-          <Navbar.Link active={path === '/' } as={'div'}>
-            <Link to='/'>
-              Home
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === '/about'} as={'div'}>
-            <Link to='/about'>
-              About
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === '/projects'} as={'div'}>
-            <Link to='/projects'>
-              Projects
-            </Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+      <Navbar.Collapse>
+        {" "}
+        {/* Collapsible Navbar section */}
+        <Navbar.Link active={path === "/"} as={"div"} className="relative">
+          <Link
+            to="/"
+            className="block py-2 px-4 text-gray-800 hover:text-blue-600 transition duration-300"
+          >
+            Home
+          </Link>
+          {path === "/" && (
+            <div className="absolute inset-x-0 bottom-0 h-0.5 bg-blue-600"></div>
+          )}
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"} className="relative">
+          <Link
+            to="/about"
+            className="block py-2 px-4 text-gray-800 hover:text-blue-600 transition duration-300"
+          >
+            About
+          </Link>
+          {path === "/about" && (
+            <div className="absolute inset-x-0 bottom-0 h-0.5 bg-blue-600"></div>
+          )}
+        </Navbar.Link>
+        <Navbar.Link
+          active={path === "/projects"}
+          as={"div"}
+          className="relative"
+        >
+          <Link
+            to="/projects"
+            className="block py-2 px-4 text-gray-800 hover:text-blue-600 transition duration-300"
+          >
+            Projects
+          </Link>
+          {path === "/projects" && (
+            <div className="absolute inset-x-0 bottom-0 h-0.5 bg-blue-600"></div>
+          )}
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
