7aa3cf5ac47a1d5272476a09e802515fbbe2a30e
maaz-official <146274279+pwbcs0933@users.noreply.github.com>
2024-03-07 00:22:25 +0500

    add mongo db and .env

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log(`Server is Runing at 3000`);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
 });
