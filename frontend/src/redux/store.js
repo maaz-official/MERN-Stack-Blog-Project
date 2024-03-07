127d75571fec124439ab3f42f4d68ad44b9c28e8
maaz-official <146274279+pwbcs0933@users.noreply.github.com>
2024-03-08 03:15:00 +0500

    add redux persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice.js';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import userReducer from './user/userSlice';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
